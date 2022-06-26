947afd53dd56eac213714e0e18d1482ee721be5a
Cullen Jennings <fluffy@iii.ca>
2022-06-26 17:20:05 -0600

    option to get relays from env var for k18s

--- src/slowRelay/slowRelay.cxx
+++ src/slowRelay/slowRelay.cxx
@@ -10,6 +10,8 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <cstdlib>
+#include <sstream>
 
 #include <slower.h>
 
@@ -25,6 +27,45 @@ int main(int argc, char* argv[]) {
   int err = slowerSetup( slower, slowerDefaultPort  );
   assert( err == 0 );
 
+  
+  // ========  Setup up upstream and relay mesh =========
+  std::list<SlowerRemote>  relays;
+  for ( int i=1; i< argc; i++ ) {
+     SlowerRemote relay;
+     err = slowerRemote( relay , argv[i] );
+     if ( err ) {
+       std::cerr << "Could not lookup IP address for relay: " << argv[i]  << std::endl;
+     } else {
+       std::clog << "Using relay at " << inet_ntoa( relay.addr.sin_addr)  << ":" <<  ntohs(relay.addr.sin_port) << std::endl;
+       relays.push_back( relay );
+     }
+  }
+  // get relays from ENV var
+  char* relayEnv = getenv( "SLOWER_RELAYS" );
+  if ( relayEnv ) {
+    std::string rs( relayEnv );
+    std::stringstream ss( rs );
+    std::vector<std::string> relayNames;
+
+    std::string buf;
+    while ( ss >> buf ) {
+      relayNames.push_back( buf );
+    }
+    
+    for ( auto r : relayNames ) {
+      SlowerRemote relay;
+      err = slowerRemote( relay , (char*)r.c_str() );
+      if ( err ) {
+        std::cerr << "Could not lookup IP address for relay: " << r  << std::endl;
+      } else {
+        std::clog << "Using relay at " << inet_ntoa( relay.addr.sin_addr)  << ":" <<  ntohs(relay.addr.sin_port) << std::endl;
+        relays.push_back( relay );
+      }
+    }
+  }
+  
+
+  // ========== MAIN Loop ==============
   Subscriptions subscribeList;
   Cache cache;
 
@@ -59,9 +100,20 @@ int main(int argc, char* argv[]) {
       assert( err == 0 );
         
       if ( !duplicate ) {
+        // add to local cache 
         std::vector<uint8_t> data( buf, buf+bufLen );
         cache.put( name,  data );
-        
+
+        // send to other relays
+        for ( SlowerRemote dest: relays ) {
+            if ( dest != remote ) {
+            std::clog << "  Sent to relay " << inet_ntoa( dest.addr.sin_addr) << ":" << ntohs( dest.addr.sin_port ) << std::endl;
+            err = slowerPub( slower, name, buf, bufLen, &dest );
+            assert( err == 0 );
+          }
+        }
+         
+        // send to anyone subscribed
         std::list<SlowerRemote> list =  subscribeList.find( name );
         for ( SlowerRemote dest: list ) {
           if ( dest != remote ) {
