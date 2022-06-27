a82f391b34a87d5f79a865f7ffe969de8cdf28d5
Richard Barnes <rlb@ipv.sx>
2022-06-27 17:15:52 -0400

    Replace security processor with Rust

---
+++ src/secProc/src/main.rs
@@ -0,0 +1,48 @@
+use qmsg_core::*;
+use std::fs::File;
+use std::time::Duration;
+
+pub const PING: MessageType = 1;
+pub const PONG: MessageType = 2;
+
+struct SecurityProcessor<'a> {
+    to_network: MessageWriter<'a, File>,
+    from_network: MessageReader<'a, File>,
+    to_ui: MessageWriter<'a, File>,
+    from_ui: MessageReader<'a, File>,
+}
+
+impl<'a> SecurityProcessor<'a> {
+    fn run(mut self) {
+        let poll_wait = Duration::from_millis(10);
+
+        loop {
+            if let Some(_msg) = self.from_network.ready(poll_wait) {
+                // TODO actions based on network messages
+            }
+
+            if let Some(_msg) = self.from_ui.ready(poll_wait) {
+                // TODO actions based on UI messages
+            }
+        }
+    }
+}
+
+fn main() {
+    // Communications with the network processor
+    let mut s2n = nonblocking::open("/tmp/pipe-s2n");
+    let mut n2s = nonblocking::open("/tmp/pipe-n2s");
+
+    // Communications with the UI processor
+    let mut s2u = nonblocking::open("/tmp/pipe-s2u");
+    let mut u2s = nonblocking::open("/tmp/pipe-u2s");
+
+    let sec_proc = SecurityProcessor {
+        to_network: MessageWriter::new(&mut s2n),
+        from_network: MessageReader::new(&mut n2s),
+        to_ui: MessageWriter::new(&mut s2u),
+        from_ui: MessageReader::new(&mut u2s),
+    };
+
+    sec_proc.run();
+}
